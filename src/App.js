2fcd038abcda522e6bb48f2f7f67f15e7cef7959
minjeongHEO <hmjoung9770@gmail.com>
2023-08-20 20:53:57 +0900

    버튼을 누르면 글 제목 바뀌는 기능의 버튼

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
 /* eslint-disable */
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const post = "강남 우동 맛집";
+  const post = '강남 우동 맛집';
   let [글제목배열, 글제목배열변경] = useState([
-    "남자 코트 추천",
-    "여자 코트 추천",
-    "간절기 아이템",
-    "인기 상품",
+    '남자 코트 추천',
+    '여자 코트 추천',
+    '간절기 아이템',
+    '인기 상품',
   ]);
   let [따봉, 따봉변경] = useState(0);
 
-  function 글제목변경하기() {
-    console.log(1);
+  function 제목변경(text, i) {
+    //글제목배열변경[i] = '여자';
+    const newArray = [...글제목배열]; // 기존 배열 복사
+    newArray[i] = '읽은 포스팅'; // 원하는 변경 값 할당
+    글제목배열변경(newArray); // 상태 변경 함수 호출
   }
   //a. state에 보관한 자료
   //b. state변경을 도와주는 함수
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{ color: "red", fontSize: "16px" }} id={post}>
+        <h4 style={{ color: 'red', fontSize: '16px' }} id={post}>
           블로그임
         </h4>
       </div>
-      //버튼을 누르면 첫 글 제목이 '여자 코트 추천'으로 바뀌는 기능의 버튼을
-      만들어봅시다.
-      {글제목배열.map((i) => (
+      {글제목배열.map((text, i) => (
         <div className="list">
           <h4>
-            <span onClick={() => 글제목배열변경(따봉 + 1)}>{i} </span>
-            <span onClick={() => 따봉변경(따봉 + 1)}>좋아요🖤</span> {따봉}
+            <span onClick={() => 제목변경(text, i)}>{text} </span>
+            <span onClick={() => 따봉변경(따봉 + 1)}>🖤👍</span> {따봉}
           </h4>
           <p>2월 17일 발행</p>
         </div>
